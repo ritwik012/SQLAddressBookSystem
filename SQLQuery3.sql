04200a338c6ffb8d95af412275ed424f9faa488a
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-12-16 14:08:31 +0530

    [Rutwik]: Added Create Table File

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -2,3 +2,7 @@
 create database AddressBookSystem;
 
 use AddressBookSystem;
+
+/*UC2*/
+create table AddressBookTable(id int identity (1,1) primary key, FirstName varchar(20), LastName varchar(20), Address varchar(50), City varchar(20), State varchar(20), Zip varchar(6), Phone varchar(16), Email varchar(40));
+exec sp_help AddressBookTable;
